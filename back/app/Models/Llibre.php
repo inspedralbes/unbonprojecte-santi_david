24960edfaf84a706b485aa2f9e8cf17189c95999
santipu03 <santipujolsau@gmail.com>
2024-03-05 10:20:46 +0100

    Crear rutes API. TG-6 #closed

--- back/app/Models/Llibre.php
+++ back/app/Models/Llibre.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Llibre extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titol',
+        'autor',
+        'any',
+        'editorial',
+        'pagines',
+        'isbn',
+        'imatge',
+        'sinopsis'
+    ];
 }
